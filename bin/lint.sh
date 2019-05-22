3d2f17827fca2c2716494633da944c6d5122f4e5
Sebastien Lavoie <github@lavoie.sl>
2019-05-22 11:34:58 +0200

    Make lint script use sh

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ ! -f ".clang-format" ]; then
     echo ".clang-format file not found!"
@@ -8,10 +8,13 @@ fi
 failures=0
 for file in $(find . -name '*.c' -o -name '*.h'); do
     echo "Checking ${file}"
-    diff -u <(clang-format -style=file "${file}") "${file}"
+    tmp="$(mktemp)"
+    clang-format -style=file "${file}" > "${tmp}"
+    diff -u "${tmp}" "${file}"
     if [[ $? -gt 0 ]]; then
         failures=$((failures + 1))
     fi
+    rm "${tmp}"
 done
 
 [[ "${failures}" -eq 0 ]] || exit 1
